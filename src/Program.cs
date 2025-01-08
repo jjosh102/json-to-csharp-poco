2e8ae5317417fde8c98ea4f83e1651a6b131352d
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2025-01-08 18:35:37 +0800

    Remove unused Toast service registration from Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,6 @@ using JsonToCsharpPoco.Converter;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddSingleton<Toas>();
 builder.Services.AddSingleton<JsonToCSharp>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
