5c3f5e7b8ffb7581a9849365beb2852db4e20dd0
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2025-01-09 15:01:00 +0800

    Add  loading screen animation

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,7 +1,18 @@
 ﻿module.exports = {
     content: ["./**/*.{razor,html,cshtml}"],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                bounceFadeIn: {
+                    '0%': { opacity: '0', transform: 'translateY(10px)' },
+                    '50%': { opacity: '1', transform: 'translateY(0)' },
+                    '100%': { opacity: '0', transform: 'translateY(-10px)' },
+                },
+            },
+            animation: {
+                'bounce-fade-in': 'bounceFadeIn 1.5s ease-in-out infinite',
+            },
+        },
     },
     darkMode: 'class',
     plugins: [],
