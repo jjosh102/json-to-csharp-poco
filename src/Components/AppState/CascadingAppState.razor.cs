8283d1470636528009b2a6edccd6adc22ae398d7
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2025-01-09 11:10:17 +0800

    Refactor theme handling in CascadingAppState and add system theme detection

--- src/Components/AppState/CascadingAppState.razor.cs
+++ src/Components/AppState/CascadingAppState.razor.cs
@@ -1,23 +1,32 @@
 
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using JsonToCsharpPoco.Components.Toast;
 
 namespace JsonToCsharpPoco.Components.AppState;
 public partial class CascadingAppState : ComponentBase
 {
+  private readonly IJSRuntime _jsRuntime;
+
+  public CascadingAppState(IJSRuntime jsRuntime) => _jsRuntime = jsRuntime;
+
   [Parameter]
   public RenderFragment? ChildContent { get; set; }
   public ToastComponent? ToastComponentService;
-
-  //todo:support local storage
-  public string CurrentTheme { get; private set; } = "light";
+  private string _currentTheme = "light";
+  protected override async Task OnInitializedAsync()
+  {
+    //todo:support local storage
+    _currentTheme = await _jsRuntime.InvokeAsync<string>("getSystemTheme");
+    await BlazorMonaco.Editor.Global.SetTheme(_jsRuntime, IsDarkTheme ? "vs-dark" : "vs-light");
+  }
 
   public void ToggleTheme()
   {
-    CurrentTheme = CurrentTheme == "light" ? "dark" : "light";
+    _currentTheme = _currentTheme == "light" ? "dark" : "light";
     StateHasChanged();
   }
 
-  public bool IsDarkTheme => CurrentTheme == "dark";
+  public bool IsDarkTheme => _currentTheme == "dark";
 
 }
