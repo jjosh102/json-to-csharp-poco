a668de31e291eb0603d15fde1620cc834dea8963
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2025-01-20 14:21:09 +0800

    Initial commit

--- src/Program.cs
+++ src/Program.cs
@@ -7,6 +7,7 @@ using Blazored.LocalStorage;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddLocalization();
 builder.Services.AddSingleton<CSharpPocoBuilder>();
 builder.Services.AddSingleton<JsonToCSharp>();
 builder.Services.AddBlazoredLocalStorageAsSingleton();
