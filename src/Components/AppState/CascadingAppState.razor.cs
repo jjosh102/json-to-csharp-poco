a31ba7dd36812b543e057e9cf44e21aa90fcca7e
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2025-02-13 08:39:42 +0800

    Set default theme to system preference

--- src/Components/AppState/CascadingAppState.razor.cs
+++ src/Components/AppState/CascadingAppState.razor.cs
@@ -8,44 +8,49 @@ using JsonToCsharpPoco.Models;
 namespace JsonToCsharpPoco.Components.AppState;
 public partial class CascadingAppState : ComponentBase
 {
-  private readonly IJSRuntime _jsRuntime;
-  private readonly ILocalStorageService _localStorageService;
+    private readonly IJSRuntime _jsRuntime;
+    private readonly ILocalStorageService _localStorageService;
 
-  public CascadingAppState(IJSRuntime jsRuntime, ILocalStorageService localStorageService)
-  {
-    _jsRuntime = jsRuntime;
-    _localStorageService = localStorageService;
-  }
-
-
-  [Parameter]
-  public RenderFragment? ChildContent { get; set; }
-  public Preferences Preferences { get; set; } = new();
-
-  protected override async Task OnInitializedAsync()
-  {
-    if (await _localStorageService.GetItemAsync<Preferences>(Constants.SavedPreferences) is { } preferences)
+    public CascadingAppState(IJSRuntime jsRuntime, ILocalStorageService localStorageService)
     {
-      Preferences = preferences;
+        _jsRuntime = jsRuntime;
+        _localStorageService = localStorageService;
     }
 
-    await BlazorMonaco.Editor.Global.SetTheme(_jsRuntime, IsDarkTheme ? "vs-dark" : "vs-light");
-  }
 
-  public async Task ToggleTheme()
-  {
-    var currentTheme = IsDarkTheme ? "light" : "dark";
-    await UpdatePreferenceAsync(t => t.CurrentTheme = currentTheme, Constants.SavedPreferences);
-    await BlazorMonaco.Editor.Global.SetTheme(_jsRuntime, IsDarkTheme ? "vs-dark" : "vs-light");
-    StateHasChanged();
-  }
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+    public Preferences Preferences { get; set; } = new();
 
-  public bool IsDarkTheme => Preferences.CurrentTheme == "dark";
+    protected override async Task OnInitializedAsync()
+    {
+        if (await _localStorageService.GetItemAsync<Preferences>(Constants.SavedPreferences) is { } preferences)
+        {
+            Preferences = preferences;
+        }
+        else
+        {
+            string systemTheme = await _jsRuntime.InvokeAsync<string>("getSystemTheme");
+            Preferences.CurrentTheme = systemTheme;
+        }
 
-  public async Task UpdatePreferenceAsync(Action<Preferences> updateAction, string storageKey)
-  {
-    updateAction(Preferences);
-    await _localStorageService.SetItemAsync(storageKey, Preferences);
-  }
+        await BlazorMonaco.Editor.Global.SetTheme(_jsRuntime, IsDarkTheme ? "vs-dark" : "vs-light");
+    }
+
+    public async Task ToggleTheme()
+    {
+        var currentTheme = IsDarkTheme ? "light" : "dark";
+        await UpdatePreferenceAsync(t => t.CurrentTheme = currentTheme, Constants.SavedPreferences);
+        await BlazorMonaco.Editor.Global.SetTheme(_jsRuntime, IsDarkTheme ? "vs-dark" : "vs-light");
+        StateHasChanged();
+    }
+
+    public bool IsDarkTheme => Preferences.CurrentTheme == "dark";
+
+    public async Task UpdatePreferenceAsync(Action<Preferences> updateAction, string storageKey)
+    {
+        updateAction(Preferences);
+        await _localStorageService.SetItemAsync(storageKey, Preferences);
+    }
 
 }
